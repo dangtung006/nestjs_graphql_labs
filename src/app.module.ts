952659b1f89564e2b97ffa0749d410756b699881
dangtung <language0512@gmail.com>
2023-02-16 10:53:42 +0700

    update graphql lesson

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloFederationDriverConfig } from '@nestjs/apollo';
+import { join} from 'path';
+import { LessonModule } from './lesson/lesson.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      playground : true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      driver : ApolloDriver,
+    }),
+  LessonModule],
   controllers: [AppController],
   providers: [AppService],
 })
